8625b69b53fdf06bb80d27d73d67f0cb8d9f901e
LeonardoLyra <leobuosi09@hotmail.com>
2021-04-13 18:59:31 +0000

    Aula 13-04

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const express = require('express');
+const{ uuid } = require('uuidv4');
 
 const server = express();
 
+//middleware
+server.use(express.json())
+
+contatos = []
+
+//POST -> Create
+//GET -> Read
+//PUT -> Update
+//DELETE -> Delete
+
 server.get('/', function(request, response) {
-    response.send('Hello World!');
+    response.json(contatos); 
+})
+
+server.get('/:id', function(request, response){
+    const id = request.param.id;
+    const result = contatos.filter(contato => contato.id == id);
+    response.json(id);
+})
+
+server.post('/', function(request, response)
+{
+    const nome = request.body.nome;
+    const telefone = request.body.telefone;
+
+    var contato = {
+        id: uuid(),
+        nome, 
+        telefone
+    };
+
+    contatos.push(contato);
+
+    response.status(201).send();
 })
 
 server.listen(process.env.PORT || 3000);
